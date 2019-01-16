a6c96e1e2e89e978e08613c1e5bf475cf4e0251c
Admin <mahesh223prasad@gmail.com>
2019-01-16 14:21:27 +0530

    Doing contacts Fragment

--- app/src/main/java/com/example/maheshprasad/whatsappclone/fragment/ContactsFragment.java
+++ app/src/main/java/com/example/maheshprasad/whatsappclone/fragment/ContactsFragment.java
@@ -2,17 +2,35 @@ package com.example.maheshprasad.whatsappclone.fragment;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.example.maheshprasad.whatsappclone.Model.Contacts;
 import com.example.maheshprasad.whatsappclone.R;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ContactsFragment extends Fragment {
+
+    private View ContactsView;
+    private RecyclerView myContectsList;
+    private DatabaseReference ContactsRef;
+    private FirebaseAuth mAuth;
+    private String currentUserId;
 
 
     public ContactsFragment() {
@@ -24,7 +42,54 @@ public class ContactsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contacts, container, false);
+        ContactsView = inflater.inflate(R.layout.fragment_contacts, container, false);
+
+        myContectsList = ContactsView.findViewById(R.id.Contacts_list);
+        myContectsList.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUserId = mAuth.getCurrentUser().getUid();
+        ContactsRef = FirebaseDatabase.getInstance().getReference().child("Contacts").child(currentUserId);
+
+        return ContactsView;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerOptions options = new FirebaseRecyclerOptions.Builder<Contacts>()
+                .setQuery(ContactsRef, Contacts.class)
+                .build();
+
+        FirebaseRecyclerAdapter<Contacts, ContactsViewHolder> adapter = new FirebaseRecyclerAdapter<Contacts, ContactsViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull ContactsViewHolder holder, int position, @NonNull Contacts model) {
+
+            }
+
+            @NonNull
+            @Override
+            public ContactsViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+
+                return null;
+            }
+        };
+
+    }
+
+    public static class ContactsViewHolder extends RecyclerView.ViewHolder {
+
+        TextView userName, userStatus;
+        CircleImageView profileImage;
+
+        public ContactsViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            userName = itemView.findViewById(R.id.display_user_name);
+            userStatus = itemView.findViewById(R.id.user_status);
+            profileImage = itemView.findViewById(R.id.users_profile_image);
+        }
     }
 
 }
