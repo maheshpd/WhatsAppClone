e00fa24499c27fe716ed5236be90d39a0d4de806
Mahesh Prasad <mahesh223prasad@gmail.com>
2021-09-10 01:04:38 +0530

    update depenencies

--- app/src/main/java/com/example/maheshprasad/whatsappclone/Activity/PhoneLoginActivity.java
+++ app/src/main/java/com/example/maheshprasad/whatsappclone/Activity/PhoneLoginActivity.java
@@ -2,8 +2,8 @@ package com.example.maheshprasad.whatsappclone.Activity;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -21,7 +21,6 @@ import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthProvider;
 
-import java.security.PrivateKey;
 import java.util.concurrent.TimeUnit;
 
 public class PhoneLoginActivity extends AppCompatActivity {
--- app/src/main/java/com/example/maheshprasad/whatsappclone/Activity/RegisterActivity.java
+++ app/src/main/java/com/example/maheshprasad/whatsappclone/Activity/RegisterActivity.java
@@ -2,8 +2,8 @@ package com.example.maheshprasad.whatsappclone.Activity;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -17,10 +17,9 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.id.FirebaseInstanceId;
 
 public class RegisterActivity extends AppCompatActivity {
 
